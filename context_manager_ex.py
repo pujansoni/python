89301db59be5776347cced829f66914e22d03cf4
pujansoni <pujansoni1997@gmail.com>
2020-04-23 13:29:14 -0400

    Adding python context_manager example

---
+++ context_manager_ex.py
@@ -0,0 +1,14 @@
+class LoggingContextManager:
+    def __enter__(self):
+        print('LoggingContextManager.__enter__()')
+        return 'You are in a with-block'
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            print('LoggingContextManager.__exit__:'
+                   'normal exit detected.')
+        else:
+            print('LoggingContextManager.__exit__'
+                   'Exception detected!'
+                   'type={}, value={}, traceback={}'.format(
+                    exc_type, exc_val, exc_tb)
